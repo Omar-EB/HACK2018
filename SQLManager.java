01b695888a3eec4b8cc9dbc0b45927db15525267
Justin <jmatt081@uottawa.ca>
2018-02-18 04:09:18 -0500

    clean-up and add of trys

--- SQLManager.java
+++ SQLManager.java
@@ -7,34 +7,34 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class SQLManager{
-	public static void createDB() throws SQLException{
-		//try {
+	public static void createDB(){
+		try {
 			Connection connection = DriverManager.getConnection("jdbc:sqlite:C:/Users/Justin/Desktop/uOttaHack.db");
 			Statement statement = connection.createStatement();
 			statement.executeUpdate("create table utable (name VARCHAR(20) PRIMARY KEY);");
 			statement.executeUpdate("create table links (URL VARCHAR(2000) NOT NULL, UID VARCHAR(20) REFERENCES uTable(name) NOT NULL);");
 			connection.close();
-		//} catch (SQLException e){}
+		} catch (SQLException e){}
 	}
 
-	public static void addUser(String name)throws SQLException{
-		//try {
+	public static void addUser(String name){
+		try {
 			Connection connection = DriverManager.getConnection("jdbc:sqlite:C:/Users/Justin/Desktop/uOttaHack.db");
 			Statement statement = connection.createStatement();
 			statement.executeUpdate("INSERT INTO utable(name) VALUES ('" + name + "');");
 			connection.close();
-		//} catch (SQLException e){}
+		} catch (SQLException e){}
 	}
 
-	public static void addLink(String name, String URL)throws SQLException{
-		//try {
+	public static void addLink(String name, String URL){
+		try {
 			Connection connection = DriverManager.getConnection("jdbc:sqlite:C:/Users/Justin/Desktop/uOttaHack.db");
 			
 			Statement statement = connection.createStatement();
 			statement.executeUpdate("INSERT INTO links(URL, UID) VALUES ('" + URL + "', '" + name + "');");
 
 			connection.close();
-		//} catch (SQLException e) {}
+		} catch (SQLException e) {}
 	}
 
 	@SuppressWarnings("unchecked")
@@ -60,14 +60,14 @@ public class SQLManager{
 		}
 	}
 
-	public static void deleteSavedLink(String name, String URL)throws SQLException{
-		//try {
+	public static void deleteSavedLink(String name, String URL){
+		try {
 			Connection connection = DriverManager.getConnection("jdbc:sqlite:C:/Users/Justin/Desktop/uOttaHack.db");
 
 			Statement statement = connection.createStatement();
 			statement.executeUpdate("DELETE FROM links WHERE (UID='" + name + "' AND URL='" + URL + "');");
 			
 			connection.close();
-		//} catch (SQLException e){}
+		} catch (SQLException e){}
 	}
 }
