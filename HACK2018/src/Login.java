1063aa506b76a87e9096c233bf7257cd8e584780
Alan Tran <alantran29@gmail.com>
2018-02-18 06:24:20 -0500

    Styling to 'Search' and copied result to 'Friend's Shared'

--- HACK2018/src/Login.java
+++ HACK2018/src/Login.java
@@ -9,10 +9,19 @@ import java.awt.FlowLayout;
 import javax.swing.JLabel;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.DropMode;
+import java.awt.Component;
+import java.awt.Dimension;
+
+import javax.swing.SwingConstants;
+import java.awt.Font;
 
 public class Login extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtNameBox;
 
 	/**
 	 * Launch the application.
@@ -37,6 +46,7 @@ public class Login extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 700, 700);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(50, 50, 50, 50));
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout());
@@ -45,10 +55,12 @@ public class Login extends JFrame {
 		contentPane.add(appName, BorderLayout.NORTH);
 		appName.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		JLabel lblAppName = new JLabel("App Name");
+		JLabel lblAppName = new JLabel("UOttaNotUseThisApp");
+		lblAppName.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		appName.add(lblAppName);
 		
 		JPanel loginLayout = new JPanel();
+		loginLayout.setBackground(Color.decode("#B2EBF2"));
 		contentPane.add(loginLayout, BorderLayout.CENTER);
 		loginLayout.setLayout(new BoxLayout(loginLayout, BoxLayout.PAGE_AXIS));
 		
@@ -77,7 +89,8 @@ public class Login extends JFrame {
 		lblNewLabel_5.setAlignmentX(CENTER_ALIGNMENT);
 		
 		
-		JLabel lblUserName = new JLabel("Alan");
+		JLabel lblUserName = new JLabel("Please Enter Your Name");
+		lblUserName.setFont(new Font("Comic Sans MS", Font.PLAIN, 16));
 		lblUserName.setAlignmentX(CENTER_ALIGNMENT);
 		loginLayout.add(lblUserName);
 		
@@ -89,8 +102,16 @@ public class Login extends JFrame {
 		loginLayout.add(lblNewLabel_7);
 		lblNewLabel_7.setAlignmentX(CENTER_ALIGNMENT);
 		
+		txtNameBox = new JTextField();
+		txtNameBox.setHorizontalAlignment(SwingConstants.CENTER);
+		txtNameBox.setText("HERE!");
+		txtNameBox.setPreferredSize(new Dimension(50, 50)) ; 
+		loginLayout.add(txtNameBox);
+		txtNameBox.setColumns(10);
+		
 		JButton btnLogin = new JButton("Login");
 		btnLogin.setAlignmentX(CENTER_ALIGNMENT);
+		btnLogin.setPreferredSize(new Dimension(700, 50));	
 		loginLayout.add(btnLogin);
 		
 		
